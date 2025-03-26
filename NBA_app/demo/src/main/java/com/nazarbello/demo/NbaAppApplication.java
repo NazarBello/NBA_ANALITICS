415da896c800bace14e3c1f03aca195fc41bd186
КВ-23 Атанов Назар <atanov.nazar@lll.kpi.ua>
2025-03-27 01:08:57 +0200

    start of the project

---
+++ NBA_app/demo/src/main/java/com/nazarbello/demo/NbaAppApplication.java
@@ -0,0 +1,13 @@
+package com.nazarbello.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaAppApplication.class, args);
+	}
+
+}
---
+++ NBA_app/demo/src/test/java/com/nazarbello/demo/NbaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazarbello.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
